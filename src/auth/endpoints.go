23f3085d962ea57e2e25e097fdba420b01adff38
TroyDota <troybensonsa@gmail.com>
2021-10-12 16:07:13 +0200

    remove auth endpoint

--- src/auth/endpoints.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import "github.com/gofiber/fiber/v2"
-
-func Listen(router fiber.Router) {
-	router.Get("/oauth2/callback/:platform", func(c *fiber.Ctx) error {
-		return nil
-	})
-}
--- src/server/v3/v3.go
+++ src/server/v3/v3.go
@@ -1,12 +1,10 @@
 package v3
 
 import (
-	"github.com/SevenTV/GQL/src/auth"
 	"github.com/SevenTV/GQL/src/global"
 	"github.com/gofiber/fiber/v2"
 )
 
 func API(gCtx global.Context, app fiber.Router) {
 	GQL(app)
-	auth.Listen(app)
 }
