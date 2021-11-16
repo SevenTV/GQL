9e065249484ea84992afb02decdcbe52d6676561
Anatole <me@anatole.dev>
2021-11-16 19:04:27 +0100

    Report Editing - status/priority/assignees

--- src/server/v3/resolvers/mutation/report.go
+++ src/server/v3/resolvers/mutation/report.go
@@ -12,6 +12,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -75,6 +76,7 @@ func (r *Resolver) CreateReport(ctx context.Context, args struct {
 	rb.SetTargetKind(args.Data.TargetKind).
 		SetTargetID(targetID).
 		SetReporterID(actor.ID).
+		SetStatus(structures.ReportStatusOpen).
 		SetSubject(args.Data.Subject).
 		SetBody(args.Data.Body).
 		SetCreatedAt(time.Now())
@@ -89,9 +91,86 @@ func (r *Resolver) CreateReport(ctx context.Context, args struct {
 	return query.CreateReportResolver(r.Ctx, ctx, nil, &returnID, query.GenerateSelectedFieldMap(ctx).Children)
 }
 
+func (r *Resolver) EditReport(ctx context.Context, args struct {
+	ReportID string
+	Data     EditReportInput
+}) (*query.ReportResolver, error) {
+	actor, ok := ctx.Value(helpers.UserKey).(*structures.User)
+	if !ok {
+		return nil, helpers.ErrUnauthorized
+	}
+	if !actor.HasPermission(structures.RolePermissionManageReports) {
+		return nil, helpers.ErrAccessDenied
+	}
+
+	// Find the report
+	report := &structures.Report{}
+	reportID, err := primitive.ObjectIDFromHex(args.ReportID)
+	if err != nil {
+		return nil, helpers.ErrBadObjectID
+	}
+	if err = r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameReports).FindOne(ctx, bson.M{"_id": reportID}).Decode(report); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, helpers.ErrUnknownEmote
+		}
+
+		logrus.WithError(err).Error("mongo")
+		return nil, err
+	}
+
+	// Apply mutations
+	rb := structures.NewReportBuilder(report)
+	if args.Data.Priority != nil { // Changing the priority of the report
+		rb.SetPriority(*args.Data.Priority)
+	}
+	if args.Data.Status != nil { // Changing report status
+		rb.SetStatus(*args.Data.Status)
+	}
+	if args.Data.Assignee != nil { // Adding or removing assignees
+		a := *args.Data.Assignee
+		assigneeID, err := primitive.ObjectIDFromHex(a[1:])
+		if err != nil {
+			return nil, helpers.ErrBadObjectID
+		}
+
+		state := a[0]
+		switch state {
+		case '+':
+			rb.AddAssignee(assigneeID)
+		case '-':
+			rb.RemoveAssignee(assigneeID)
+		default:
+			return nil, fmt.Errorf("invalid operator for assignee mutation")
+		}
+	}
+
+	// Do update
+	if err = r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameReports).FindOneAndUpdate(ctx, bson.M{
+		"_id": reportID,
+	}, rb.Update, options.FindOneAndUpdate().SetReturnDocument(options.After)).Decode(report); err != nil {
+		return nil, err
+	}
+
+	return query.CreateReportResolver(r.Ctx, ctx, report, &report.ID, query.GenerateSelectedFieldMap(ctx).Children)
+}
+
 type CreateReportInput struct {
 	TargetKind structures.ReportTargetKind `json:"target_kind"`
 	TargetID   string                      `json:"target_id"`
 	Subject    string                      `json:"subject"`
 	Body       string                      `json:"body"`
+}
+
+type EditReportInput struct {
+	Priority *int32                   `json:"priority"`
+	Status   *structures.ReportStatus `json:"status"`
+	Assignee *string                  `json:"assignee"`
+	Note     *EditReportNoteInput     `json:"note"`
+}
+
+type EditReportNoteInput struct {
+	Timestamp time.Time `json:"timestamp"`
+	Content   *string   `json:"content"`
+	Internal  *bool     `json:"internal"`
+	Reply     *string   `json:"reply"`
 }
