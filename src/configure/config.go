ed4aadbf6e32793f71f54b7c7a7426a45acd8743
TroyDota <troybensonsa@gmail.com>
2021-11-26 13:02:50 +0200

    add command line params

--- src/configure/config.go
+++ src/configure/config.go
@@ -6,12 +6,64 @@ import (
 	"strings"
 
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
+func checkErr(err error) {
+	if err != nil {
+		logrus.WithError(err).Fatal("config")
+	}
+}
+
+func New() *Config {
+	config := viper.New()
+
+	// Default config
+	b, _ := json.Marshal(Config{
+		ConfigFile: "config.yaml",
+	})
+	tmp := viper.New()
+	defaultConfig := bytes.NewReader(b)
+	tmp.SetConfigType("json")
+	checkErr(tmp.ReadConfig(defaultConfig))
+	checkErr(config.MergeConfigMap(viper.AllSettings()))
+
+	pflag.String("config", "config.yaml", "Config file location")
+	pflag.Bool("noheader", false, "Disable the startup header")
+	pflag.Parse()
+	checkErr(config.BindPFlags(pflag.CommandLine))
+
+	// File
+	config.SetConfigFile(config.GetString("config"))
+	config.AddConfigPath(".")
+	err := config.ReadInConfig()
+	if err != nil {
+		logrus.Warning(err)
+		logrus.Info("Using default config")
+	} else {
+		checkErr(config.MergeInConfig())
+	}
+
+	// Environment
+	replacer := strings.NewReplacer(".", "_")
+	config.SetEnvKeyReplacer(replacer)
+	config.SetEnvPrefix("7TV")
+	config.AllowEmptyEnv(true)
+	config.AutomaticEnv()
+
+	// Print final config
+	c := &Config{}
+	checkErr(config.Unmarshal(&c))
+
+	initLogging(c.Level)
+
+	return c
+}
+
 type Config struct {
 	Level      string `mapstructure:"level" json:"level"`
-	ConfigFile string `mapstructure:"config_file" json:"config_file"`
+	ConfigFile string `mapstructure:"config" json:"config"`
 	NoHeader   bool   `mapstructure:"noheader" json:"noheader"`
 	WebsiteURL string `mapstructure:"website_url" json:"website_url"`
 	NodeName   string `mapstructure:"node_name" json:"node_name"`
@@ -46,50 +98,4 @@ type Config struct {
 	Credentials struct {
 		JWTSecret string `mapstructure:"jwt_secret" json:"jwt_secret"`
 	} `mapstructure:"credentials" json:"credentials"`
-}
-
-func checkErr(err error) {
-	if err != nil {
-		logrus.WithError(err).Fatal("config")
-	}
-}
-
-func New() *Config {
-	config := viper.New()
-
-	// Default config
-	b, _ := json.Marshal(Config{
-		ConfigFile: "config.yaml",
-	})
-	tmp := viper.New()
-	defaultConfig := bytes.NewReader(b)
-	tmp.SetConfigType("json")
-	checkErr(tmp.ReadConfig(defaultConfig))
-	checkErr(config.MergeConfigMap(viper.AllSettings()))
-
-	// File
-	config.SetConfigFile(config.GetString("config_file"))
-	config.AddConfigPath(".")
-	err := config.ReadInConfig()
-	if err != nil {
-		logrus.Warning(err)
-		logrus.Info("Using default config")
-	} else {
-		checkErr(config.MergeInConfig())
-	}
-
-	// Environment
-	replacer := strings.NewReplacer(".", "_")
-	config.SetEnvKeyReplacer(replacer)
-	config.SetEnvPrefix("7TV")
-	config.AllowEmptyEnv(true)
-	config.AutomaticEnv()
-
-	// Print final config
-	c := &Config{}
-	checkErr(config.Unmarshal(&c))
-
-	initLogging(c.Level)
-
-	return c
 }
