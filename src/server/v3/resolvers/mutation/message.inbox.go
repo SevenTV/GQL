92cdb366daa43662548adf52e70e914096b122e0
Anatole <me@anatole.dev>
2021-11-29 10:24:53 +0100

    Inbox & Comments (#15)
    * Message Resolver
    
    * mutation - SendInboxMessage
    
    * Correct recipients query
    
    * Fix panic at MessageResolver
    
    * Inbox query
    
    * "Read Messages" mutation

---
+++ src/server/v3/resolvers/mutation/message.inbox.go
@@ -0,0 +1,138 @@
+package mutation
+
+import (
+	"context"
+	"time"
+
+	"github.com/SevenTV/Common/mongo"
+	"github.com/SevenTV/Common/structures"
+	"github.com/SevenTV/Common/utils"
+	"github.com/SevenTV/GQL/src/server/v3/helpers"
+	"github.com/SevenTV/GQL/src/server/v3/resolvers/query"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (r *Resolver) ReadMessages(ctx context.Context, args struct {
+	MessageIDs []string
+	Read       *bool
+}) (*int32, error) {
+	// Get the actor user
+	actor, ok := ctx.Value(helpers.UserKey).(*structures.User)
+	if !ok {
+		return nil, helpers.ErrAccessDenied
+	}
+
+	// Parse message IDs
+	messageIDs := []primitive.ObjectID{}
+	for _, s := range args.MessageIDs {
+		if !primitive.IsValidObjectID(s) {
+			continue
+		}
+		id, _ := primitive.ObjectIDFromHex(s)
+		messageIDs = append(messageIDs, id)
+	}
+
+	// Update the readstates
+	value := true
+	if args.Read != nil && !*args.Read {
+		value = false
+	}
+	result, err := r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameMessagesRead).UpdateMany(ctx, bson.M{
+		"recipient_id": actor.ID,
+		"message_id":   bson.M{"$in": messageIDs},
+	}, bson.M{
+		"$set": bson.M{"read": value},
+	})
+	if err != nil {
+		logrus.WithError(err).Error("mongo")
+		return nil, err
+	}
+
+	return utils.Int32Pointer(int32(result.ModifiedCount)), nil
+}
+
+func (r *Resolver) SendInboxMessage(ctx context.Context, args struct {
+	Recipients []string
+	Subject    string
+	Content    string
+	Important  *bool
+	Anonymous  *bool
+}) (*query.MessageResolver, error) {
+	// Get the actor user
+	actor, ok := ctx.Value(helpers.UserKey).(*structures.User)
+	if !ok {
+		return nil, helpers.ErrAccessDenied
+	}
+	// The actor must be allowed the "Send Messages" permission
+	if !actor.HasPermission(structures.RolePermissionSendMessages) {
+		return nil, helpers.ErrAccessDenied
+	}
+
+	// Parse recipient IDs
+	recipientIDs := []primitive.ObjectID{}
+	for _, s := range args.Recipients {
+		if !primitive.IsValidObjectID(s) {
+			continue
+		}
+		id, _ := primitive.ObjectIDFromHex(s)
+		recipientIDs = append(recipientIDs, id)
+	}
+
+	// Find recipients
+	recipients := []*structures.User{}
+	cur, err := r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameUsers).Find(ctx, bson.M{
+		"$and": bson.A{
+			bson.M{"_id": recipientIDs},
+			bson.M{"_id": bson.M{"$not": bson.M{"$eq": actor.BlockedUserIDs}}},
+		},
+	})
+	if err != nil {
+		logrus.WithError(err).Error("mongo")
+		return nil, err
+	}
+	if err = cur.All(ctx, &recipients); err != nil {
+		logrus.WithError(err).Error("mongo")
+		return nil, err
+	}
+
+	// Create the message
+	anon := utils.Ternary(args.Anonymous != nil && *args.Anonymous, true, false).(bool)
+	important := utils.Ternary(args.Important != nil && *args.Important, true, false).(bool)
+	mb := structures.NewMessageBuilder(&structures.Message{}).
+		SetKind(structures.MessageKindInbox).
+		SetAnonymous(anon).
+		SetTimestamp(time.Now()).
+		SetAuthorID(actor.ID).
+		AsInbox(structures.MessageDataInbox{
+			Subject:   args.Subject,
+			Content:   args.Content,
+			Important: important,
+		})
+
+	// Write message to DB
+	result, err := r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameMessages).InsertOne(ctx, mb.Message)
+	if err != nil {
+		logrus.WithError(err).WithField("actor_id", actor.ID).Error("mongo, failed to create message")
+		return nil, err
+	}
+	msgID := result.InsertedID.(primitive.ObjectID)
+
+	// Create read states for the recipients
+	w := make([]mongo.WriteModel, len(recipientIDs))
+	for i, id := range recipientIDs {
+		w[i] = &mongo.InsertOneModel{
+			Document: &structures.MessageRead{
+				MessageID:   msgID,
+				RecipientID: id,
+				Read:        false,
+			},
+		}
+	}
+	if _, err = r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameMessagesRead).BulkWrite(ctx, w); err != nil {
+		logrus.WithError(err).WithField("message_id", result.InsertedID).Error("mongo, couldn't create a read state for message")
+	}
+
+	return query.CreateMessageResolver(r.Ctx, ctx, nil, &msgID, query.GenerateSelectedFieldMap(ctx).Children)
+}
---
+++ src/server/v3/resolvers/query/message.go
@@ -0,0 +1,120 @@
+package query
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/SevenTV/Common/mongo"
+	"github.com/SevenTV/Common/structures"
+	"github.com/SevenTV/GQL/src/global"
+	"github.com/SevenTV/GQL/src/server/v3/helpers"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type MessageResolver struct {
+	ctx context.Context
+	*structures.MessageBuilder
+
+	fields map[string]*SelectedField
+	gCtx   global.Context
+}
+
+func CreateMessageResolver(gCtx global.Context, ctx context.Context, msg *structures.Message, msgID *primitive.ObjectID, fields map[string]*SelectedField) (*MessageResolver, error) {
+	mb := structures.NewMessageBuilder(msg)
+
+	var pipeline mongo.Pipeline
+	if msg == nil && msgID == nil {
+		return nil, fmt.Errorf("unresolvable")
+	}
+	if msg == nil {
+		msg = &structures.Message{}
+		mb.Message = msg
+		pipeline = mongo.Pipeline{
+			{{
+				Key:   "$match",
+				Value: bson.M{"_id": msgID},
+			}},
+		}
+	} else {
+		pipeline = mongo.Pipeline{
+			{{
+				Key:   "$replaceRoot",
+				Value: bson.M{"newRoot": msg},
+			}},
+		}
+	}
+
+	// Relation: author
+	if _, ok := fields["author"]; ok && msg.Author == nil {
+		pipeline = append(pipeline, []bson.D{
+			{{
+				Key: "$lookup",
+				Value: mongo.Lookup{
+					From:         mongo.CollectionNameUsers,
+					LocalField:   "author_id",
+					ForeignField: "_id",
+					As:           "_author",
+				},
+			}},
+			{{Key: "$set", Value: bson.M{"author": bson.M{"$first": "$_author"}}}}, {{Key: "$unset", Value: bson.A{"_author"}}},
+		}...)
+	}
+
+	if mb.Message.ID.IsZero() || len(pipeline) > 1 {
+		cur, err := gCtx.Inst().Mongo.Collection(mongo.CollectionNameMessages).Aggregate(ctx, pipeline)
+		if err != nil {
+			return nil, err
+		}
+		cur.Next(ctx)
+		cur.Close(ctx)
+		if err = cur.Decode(mb.Message); err != nil {
+			logrus.WithError(err).Error("mongo")
+			return nil, err
+		}
+	}
+
+	return &MessageResolver{
+		ctx:            ctx,
+		MessageBuilder: mb,
+		fields:         fields,
+		gCtx:           gCtx,
+	}, nil
+}
+
+func (r *MessageResolver) ID() string {
+	return r.Message.ID.Hex()
+}
+
+func (r *MessageResolver) Kind() structures.MessageKind {
+	return r.Message.Kind
+}
+
+func (r *MessageResolver) CreatedAt() string {
+	return r.Message.CreatedAt.Format(time.RFC3339)
+}
+
+func (r *MessageResolver) Author(ctx context.Context) (*UserResolver, error) {
+	if r.Message.Author == nil {
+		return nil, nil
+	}
+	// Omit the author if message is anonymous
+	// (Unless the user is privileged)
+	if r.Message.Anonymous {
+		actor := ctx.Value(helpers.UserKey).(*structures.User)
+		if actor == nil {
+			return nil, nil
+		}
+		if !actor.HasPermission(structures.RolePermissionBypassPrivacy) {
+			return nil, nil
+		}
+	}
+
+	return CreateUserResolver(r.gCtx, ctx, r.Message.Author, &r.Message.AuthorID, r.fields)
+}
+
+func (r *MessageResolver) Data() string {
+	return r.Message.Data.String()
+}
