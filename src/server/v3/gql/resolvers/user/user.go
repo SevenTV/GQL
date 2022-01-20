c22d78f547042d92e03fff9d07faa2f62bd068ab
Anatole <me@anatole.dev>
2022-01-20 11:33:15 +0100

    Remove some unused user resolver funcs

--- src/server/v3/gql/resolvers/user/user.go
+++ src/server/v3/gql/resolvers/user/user.go
@@ -17,21 +17,9 @@ func New(r types.Resolver) generated.UserResolver {
 	return &Resolver{r}
 }
 
-func (r *Resolver) Roles(ctx context.Context, obj *model.User) ([]*model.Role, error) {
-	return obj.Roles, nil
-}
-
-func (r *Resolver) Editors(ctx context.Context, obj *model.User) ([]*model.UserEditor, error) {
-	return nil, nil
-}
-
 func (r *Resolver) OwnedEmotes(ctx context.Context, obj *model.User) ([]*model.Emote, error) {
 	// TODO
 	return nil, nil
-}
-
-func (r *Resolver) Connections(ctx context.Context, obj *model.User) ([]*model.UserConnection, error) {
-	return obj.Connections, nil
 }
 
 func (r *Resolver) InboxUnreadCount(ctx context.Context, obj *model.User) (int, error) {
