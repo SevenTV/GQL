1594d5ce97c3be2c500211c6966679e9d1d8d1bb
Anatole <me@anatole.dev>
2022-02-11 10:35:43 +0100

    Pre-query active sets

--- src/server/v3/gql/resolvers/emote/emote.channels.go
+++ src/server/v3/gql/resolvers/emote/emote.channels.go
@@ -2,15 +2,23 @@ package emote
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/SevenTV/Common/errors"
 	"github.com/SevenTV/Common/mongo"
 	"github.com/SevenTV/Common/structures/v3"
 	"github.com/SevenTV/Common/structures/v3/aggregations"
+	"github.com/SevenTV/Common/utils"
 	"github.com/SevenTV/GQL/graph/model"
 	"github.com/SevenTV/GQL/src/server/v3/gql/helpers"
+	"github.com/hashicorp/go-multierror"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const EMOTE_CHANNEL_QUERY_SIZE_MOST = 25
@@ -38,56 +46,62 @@ func (r *Resolver) Channels(ctx context.Context, obj *model.Emote, pageArg *int,
 		}).SetDetail("No further pagination is allowed")
 	}
 
-	pipeline := mongo.Pipeline{
-		{{
-			Key:   "$sort",
-			Value: bson.M{"metadata.role_position": -1},
-		}},
-		{{Key: "$skip", Value: (page - 1) * limit}},
-		{{
-			Key:   "$unwind",
-			Value: bson.M{"path": "$connections"},
-		}},
-		{{
-			Key: "$lookup",
-			Value: mongo.LookupWithPipeline{
-				From: mongo.CollectionNameEmoteSets,
-				Let:  bson.M{"set_id": "$connections.emote_set_id"},
-				Pipeline: &mongo.Pipeline{
-					{{
-						Key: "$match",
-						Value: bson.M{
-							"emotes.id": obj.ID,
-							"$expr": bson.M{
-								"$eq": bson.A{"$_id", "$$set_id"},
-							},
-						},
-					}},
-					{{Key: "$project", Value: bson.M{"_id": 1}}},
-				},
-				As: "sets",
-			},
-		}},
-		{{Key: "$unset", Value: "connections"}},
-		{{
-			Key: "$set",
-			Value: bson.M{"ok": bson.M{
-				"$cond": bson.M{
-					"if":   bson.M{"$gt": bson.A{bson.M{"$size": "$sets"}, 0}},
-					"then": true,
-					"else": false,
-				},
-			}},
-		}},
-		{{Key: "$match", Value: bson.M{"ok": true}}},
-		{{Key: "$limit", Value: limit}},
+	// Fetch emote sets that have this emote
+	setIDs := []primitive.ObjectID{}
+
+	// Ping redis for a cached value
+	rKey := r.Ctx.Inst().Redis.ComposeKey("gql-v3", fmt.Sprintf("emote:%s:active_sets", obj.ID.Hex()))
+	v, err := r.Ctx.Inst().Redis.Get(ctx, rKey)
+	if err == nil && v != "" {
+		if err = json.Unmarshal(utils.S2B(v), &setIDs); err != nil {
+			logrus.WithError(err).Error("couldn't decode emote's active set ids")
+		}
+	} else {
+		cur, err := r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameEmoteSets).Find(ctx, bson.M{"emotes.id": obj.ID}, options.Find().SetProjection(bson.M{"owner_id": 1}))
+		if err != nil {
+			return nil, err
+		}
+		for i := 0; cur.Next(ctx); i++ {
+			v := &structures.EmoteSet{}
+			if err = cur.Decode(v); err != nil {
+				logrus.WithError(err).Error("mongo, couldn't decode into EmoteSet")
+			}
+			setIDs = append(setIDs, v.ID)
+		}
+
+		// Set in redis
+		b, err := json.Marshal(setIDs)
+		if err = multierror.Append(err, r.Ctx.Inst().Redis.SetEX(ctx, rKey, utils.B2S(b), time.Hour*6)).ErrorOrNil(); err != nil {
+			logrus.WithError(err).Error("failed to cache set ids in redis")
+		}
 	}
 
-	cur, err := r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameUsers).Aggregate(ctx, aggregations.Combine(pipeline, aggregations.UserRelationRoles))
+	// Fetch users with this set active
+	cur, err := r.Ctx.Inst().Mongo.Collection(mongo.CollectionNameUsers).Aggregate(ctx, aggregations.Combine(
+		mongo.Pipeline{
+			{{
+				Key:   "$sort",
+				Value: bson.M{"metadata.role_position": -1},
+			}},
+			{{
+				Key: "$match",
+				Value: bson.M{
+					"connections.emote_set_id": bson.M{
+						"$in": setIDs,
+					},
+				},
+			}},
+			{{Key: "$skip", Value: (page - 1) * limit}},
+			{{
+				Key:   "$limit",
+				Value: limit,
+			}},
+		},
+		aggregations.UserRelationRoles,
+	))
 	if err != nil {
 		return nil, err
 	}
-
 	users := []*structures.User{}
 	if err = cur.All(ctx, &users); err != nil {
 		return nil, err
