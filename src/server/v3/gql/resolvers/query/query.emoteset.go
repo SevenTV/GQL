3493db207bff3141626b87e05a98a61537e9ce23
Anatole <me@anatole.dev>
2022-02-14 16:35:12 +0100

    Named Emote Sets (+GLOBAL)

--- src/server/v3/gql/resolvers/query/query.emoteset.go
+++ src/server/v3/gql/resolvers/query/query.emoteset.go
@@ -3,6 +3,7 @@ package query
 import (
 	"context"
 
+	"github.com/SevenTV/Common/errors"
 	"github.com/SevenTV/GQL/graph/model"
 	"github.com/SevenTV/GQL/src/server/v3/gql/loaders"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -10,4 +11,20 @@ import (
 
 func (r *Resolver) EmoteSet(ctx context.Context, id primitive.ObjectID) (*model.EmoteSet, error) {
 	return loaders.For(ctx).EmoteSetByID.Load(id)
+}
+
+func (r *Resolver) NamedEmoteSet(ctx context.Context, name model.EmoteSetName) (*model.EmoteSet, error) {
+	var setID primitive.ObjectID
+
+	switch name {
+	case model.EmoteSetNameGlobal:
+		sys := r.Ctx.Inst().Mongo.System(ctx)
+		setID = sys.EmoteSetID
+	}
+
+	if setID.IsZero() {
+		return nil, errors.ErrUnknownEmoteSet()
+	}
+
+	return loaders.For(ctx).EmoteSetByID.Load(setID)
 }
