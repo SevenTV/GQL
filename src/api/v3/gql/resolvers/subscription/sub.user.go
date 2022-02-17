a63d7ef391cef174c13a6b316798b15c5c12e2b6
Anatole <me@anatole.dev>
2022-02-17 14:01:17 +0100

    User subscription resolver

--- src/api/v3/gql/resolvers/subscription/sub.user.go
+++ src/api/v3/gql/resolvers/subscription/sub.user.go
@@ -9,6 +9,7 @@ import (
 	"github.com/SevenTV/GQL/src/api/v3/gql/auth"
 	"github.com/SevenTV/GQL/src/api/v3/gql/loaders"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // var json = jsoniter.ConfigCompatibleWithStandardLibrary
@@ -43,6 +44,40 @@ func (r *Resolver) CurrentUser(ctx context.Context) (<-chan *model.User, error) 
 		sub := r.subscribe(ctx, "users", actor.ID)
 		for range sub {
 			user, _ = getUser()
+			if user != nil {
+				ch <- user
+			}
+		}
+	}()
+
+	return ch, nil
+}
+
+func (r *Resolver) User(ctx context.Context, id primitive.ObjectID) (<-chan *model.User, error) {
+	getUser := func() (*model.User, error) {
+		user, err := loaders.For(ctx).UserByID.Load(id)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return nil, errors.ErrUnknownEmote()
+			}
+
+			logrus.WithError(err).Error("failed to subscribe")
+			return nil, errors.ErrInternalServerError().SetDetail(err.Error())
+		}
+		return user, nil
+	}
+
+	user, err := getUser()
+	if err != nil {
+		return nil, err
+	}
+	ch := make(chan *model.User, 1)
+	ch <- user
+
+	go func() {
+		sub := r.subscribe(ctx, "users", id)
+		for range sub {
+			user, _ := getUser()
 			if user != nil {
 				ch <- user
 			}
